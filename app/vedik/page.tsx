1a56cc8629509898c5bfbb0608a4612b05e6c8f1
vedikgannoji <vedikgannoji5126@gmail.com>
2025-06-07 17:22:40 +0530

    Added new page: VedikPage

---
+++ app/vedik/page.tsx
@@ -0,0 +1,8 @@
+
+export default function VedikPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">hello</h1>
+    </div>
+  );
+} 
