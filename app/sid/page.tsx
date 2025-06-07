d1e8c538b750bb0b7dbbb73e714e4dfa4609fcc9
Sidsmartz <siddhartht4206@gmail.com>
2025-06-07 16:33:28 +0530

    I created a page called sid

---
+++ app/sid/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function SidPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello sid</h1>
+      <h2 className="text-2xl font-mono text-center text-red-600">This is my sid page</h2>
+    </div>
+  )
+} 
